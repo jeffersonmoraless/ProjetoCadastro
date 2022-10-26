26f6fb31cc7b09c55f416a7522a0ffe6d33b07a5
Jefferson Morales <105823338+jeffersonmoraless@users.noreply.github.com>
2022-10-26 12:56:20 -0300

    atualização usando querySelector

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-setTimeout(()=>{
+/*setTimeout(()=>{
     alert ("hello word");  
-},10000);
+},10000);*/
+var name = document.querySelector("#exampleInputName");
+var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
+
+var birth = document.querySelector("#exampleInputBirth");
+var country = document.querySelector("#exampleInputCountry");
+var email = document.querySelector("#exampleInputEmail");
+var password = document.querySelector("#exampleInputPassword");
+var photo = document.querySelector("#exampleInputFile");
+var password = document.querySelector("#exampleInputPassword1");
+//nome.value = 'jefferson';
