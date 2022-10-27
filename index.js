d2db181ada3f7b220d17eebef699994f37622a59
Jefferson Morales <105823338+jeffersonmoraless@users.noreply.github.com>
2022-10-27 15:57:12 -0300

    alteração27/10/2022

--- index.js
+++ index.js
@@ -3,11 +3,28 @@
 },10000);*/
 var name = document.querySelector("#exampleInputName");
 var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
-
 var birth = document.querySelector("#exampleInputBirth");
 var country = document.querySelector("#exampleInputCountry");
 var email = document.querySelector("#exampleInputEmail");
 var password = document.querySelector("#exampleInputPassword");
 var photo = document.querySelector("#exampleInputFile");
-var password = document.querySelector("#exampleInputPassword1");
-//nome.value = 'jefferson';
+var admin = document.querySelector("#exampleInputAdmin");
+
+var fields = document.querySelectorAll("#form-user-create [name]");
+var user = {};
+
+fields.forEach(function(campo,index){
+    
+    if(campo.name == "gender"){
+        
+        user[campo.name] = campo.value;
+    
+    }
+    else{
+
+        user[campo.name] = campo.value;
+    
+    }
+    
+});
+console.log("abc",user);
